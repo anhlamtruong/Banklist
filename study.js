3ed532f0bb1f6afedf7bf61c2989af18c17d96cb
STSC <STSC@LENOVO-127800>
2022-08-24 12:37:59 -0700

    Updating functionality

---
+++ study.js
@@ -0,0 +1,43 @@
+'use strict';
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     if (isEven(i)) {
+//       row.style.backgroundColor = 'orangered';
+//     } else {
+//       row.style.backgroundColor = 'blue';
+//     }
+//   });
+// });
+
+// const diameter = 287_460_000_000;
+// console.log(diameter);
+
+// const price = 345_99;
+// console.log(price);
+
+// const transferFee1 = 15_00;
+// const transferFee2 = 1_500;
+
+// console.log(Number(''));
+
+//Create a date
+
+// const now = new Date();
+// console.log(new Date(account1.movementsDates[0]));
+// // console.log(new Date(account1.mov));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDate());
+// console.log(future.getDay());
+// console.log(future.getHours());
+// console.log(future.getMinutes());
+// console.log(future.getSeconds());
+// console.log(future.toISOString());
+// console.log(future.getTime());
+// console.log(Date.now());
